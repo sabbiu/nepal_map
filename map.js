9547ffc41464df53dfacf6c45861ac0aa9a75c94
Sabbiu Shah <Sabbiu Shah>
2016-11-20 21:57:08 +0545

    color update

--- map.js
+++ map.js
@@ -1,61 +1,84 @@
 var di_url = "http://apps.geoportal.icimod.org/ArcGIS/rest/services/Nepal/District/MapServer";
 var vd_url = "http://apps.geoportal.icimod.org/ArcGIS/rest/services/Nepal/VDC/MapServer";
+var chk = "https://services.arcgis.com/V6ZHFr6zdgNZuVG0/ArcGIS/rest/services/NYCDemographics1/FeatureServer/0";
 
 require([
-    "esri/Map",
-    "esri/views/MapView",
-    "esri/layers/MapImageLayer",
-    "esri/renderers/SimpleRenderer",
+        "esri/Map",
+        "esri/views/MapView",
+        "esri/views/SceneView",
+
+        "esri/layers/FeatureLayer",
+        "esri/renderers/SimpleRenderer",
     "esri/symbols/SimpleFillSymbol",
     "esri/symbols/SimpleLineSymbol",
     "esri/Color",
-    "dojo/domReady!"
-  ],
-  function(
-    Map, MapView, MapImageLayer, SimpleRenderer, SimpleFillSymbol, SimpleLineSymbol, Color
-  ) {
-    
-
-    var permitsLyr = new MapImageLayer({
-      url: "http://apps.geoportal.icimod.org/ArcGIS/rest/services/Nepal/District/MapServer",
-
-      sublayers: [
-        {
-          id: 1,
-          visible: true,
-        },
-        {
-          id: 0,
-          visible: true,
-        }
+    "dojo/on",
+        "dojo/domReady!"
       ],
+      function(
+        Map, MapView,SceneView,
+        FeatureLayer,
+        SimpleRenderer, SimpleFillSymbol, SimpleLineSymbol, Color,
+        on
+      ) {
 
-    });
-    
-    /*****************************************************************
-     * Add the layer to a map
-     *****************************************************************/
-    var map = new Map({
-      // basemap: "dark-gray",
-      layers: [permitsLyr]
-    });
+        // var featureLayer = new FeatureLayer({
+        //   url: di_url,
+          
+        //   id: 'dis',
+        //   opacity: 0.5,
+        //   visible: false,
+        //   layerId: 0,
+        // });
+        
+        var featureLayer = new FeatureLayer({
+          url: di_url,
+          renderer: new SimpleRenderer({
+            symbol: new SimpleFillSymbol({
+              style: SimpleFillSymbol.STYLE_SOLID,
+              color: new Color([0,128,0, 0.5]),
+            })
+          }),
+          id: 'nam',
+          opacity: 1,
+          visible: true,
+          layerId: 1,
+        });
 
-    var view = new MapView({
-      container: "viewDiv",
-      map: map
-    });
+        var map = new Map({
+          // basemap: "hybrid"
+          layers: [
+            featureLayer
+            // featureLayer2
+          ]
+        });
+        // map.add(featureLayer2);
 
-    permitsLyr.setRenderer(new SimpleRenderer(new SimpleLineSymbol(
-                        SimpleLineSymbol.STYLE_SOLID,
-                        new Color([0,128,0, 1]),3)));
+        // featureLayer.renderer = 
 
-    /*****************************************************************
-     * Animate to the layer's full extent when the layer loads.
-     *****************************************************************/
-    permitsLyr.then(function() {
-      view.goTo(permitsLyr.fullExtent);
-      
-    });
+        var view = new MapView ({
+          container: "viewDiv",
+          map: map,
+        });
 
+        /********************
+         * Add feature layer
+         ********************/
 
-  });
+        featureLayer.then(function(){
+          view.goto(featureLayer.fullExtent);
+          // featureLayer2.labelsVisible = true;
+          // featureLayer.visible = false;
+        })
+
+        view.on("layerview-create", function(event) {
+          if (event.layer.id === "dis") {
+            // Explore the properties of the housing layer's layer view here
+            console.log("LayerView for district!", event.layerView);
+          }
+          if (event.layer.id === "nam") {
+            // Explore the properties of the transportation layer's layer view here
+            console.log("LayerView for name!", event.layerView);
+          }
+        });
+      });
